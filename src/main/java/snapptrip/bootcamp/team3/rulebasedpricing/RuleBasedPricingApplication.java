76a1b51ece74fd42eca2dcd3799aeccb66a95cbe
momen7899 <momenhamaveisi7@gmail.com>
2022-08-18 12:01:15 +0430

    Create New project

---
+++ src/main/java/snapptrip/bootcamp/team3/rulebasedpricing/RuleBasedPricingApplication.java
@@ -0,0 +1,13 @@
+package snapptrip.bootcamp.team3.rulebasedpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleBasedPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleBasedPricingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/snapptrip/bootcamp/team3/rulebasedpricing/RuleBasedPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package snapptrip.bootcamp.team3.rulebasedpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleBasedPricingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
